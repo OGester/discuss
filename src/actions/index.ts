6dd9e427dca9ab6528dffa80c785d5efa88e04ff
Oskar Gester <gester.oskar@gmail.com>
2025-07-29 21:33:50 +0200

    Oauth setup completed github client id and secret has been created on github

---
+++ src/actions/index.ts
@@ -0,0 +1,12 @@
+"use server";
+
+import * as auth from "@/auth";
+
+export async function signIn() {
+  // specifying the oauth providers, in this case just github, more can be added in the future
+  return auth.signIn("github");
+}
+
+export async function signOut() {
+  return auth.signOut();
+}
