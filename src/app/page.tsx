df7627f2a60ac783e43a0b47419dda1c9618a45d
Oskar Gester <gester.oskar@gmail.com>
2025-07-30 22:05:36 +0200

    tested accessing user informatio both from server component and client component with success

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@nextui-org/react";
 import * as actions from "@/actions";
 import { auth } from "@/auth";
+import Profile from "@/components/profile";
 
 export default async function Home() {
   const session = await auth();
@@ -15,7 +16,11 @@ export default async function Home() {
         <Button type="submit">Sign Out!</Button>
       </form>
 
+      {/* test to validate the user info is present as expected
+        <div>{JSON.stringify(session.user)}</div> */}
       {session?.user ? <div>Signed In</div> : <div>Signed Out</div>}
+
+      <Profile />
     </div>
   );
 }
