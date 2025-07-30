7f9822af01b2e224b8ee68ed727fdb4fd0fac028
Oskar Gester <gester.oskar@gmail.com>
2025-07-30 21:43:43 +0200

    Sign in and sign out with Oauth tested and working

--- src/auth.ts
+++ src/auth.ts
@@ -4,7 +4,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "@/db";
 
 const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
-const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_ID;
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 
 if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET) {
   throw new Error("Missing github oauth credentials");
