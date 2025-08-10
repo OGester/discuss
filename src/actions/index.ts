e9de1c1ba7c4f94bfb153fbd2d3c5b114ae385f5
Oskar Gester <gester.oskar@gmail.com>
2025-08-10 21:12:49 +0200

    initial design structure created, routing folders and server action files prepped

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,12 +1,5 @@
-"use server";
-
-import * as auth from "@/auth";
-
-export async function signIn() {
-  // specifying the oauth providers, in this case just github, more can be added in the future
-  return auth.signIn("github");
-}
-
-export async function signOut() {
-  return auth.signOut();
-}
+export { signIn } from "./signIn";
+export { signOut } from "./signOut";
+export { createComment } from "./createComment";
+export { createPost } from "./createPost";
+export { createTopic } from "./createTopic";
